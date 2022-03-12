594a655bd805eb2e17f881260dd5f77799746976
rhglatts <rhglatts@gmail.com>
2022-03-11 19:52:31 -0800

    90% done with evaluatePostFix

--- main/Calc.java
+++ main/Calc.java
@@ -1,61 +1,94 @@
 package project2;
 
 /**
+ * Contains methods convertToPostfix, which converts infix expressions to
+ * postfix and evaluatePostfix which evaluates the value of a postfix
+ * expression given values of variables
+ * 
  * @author Rebecca Glatts, Yvonne Li, Jessica Ortega
  * 
  */
 public class Calc {
 
-    //basic algorithm from 5.18 
-    static Algorithm evaluatePostfix(postfix){
+    /** Given a postfix expression, it evaluates it using the values of the variables 
+     * and returns the value of the evaluated expression
+     * 
+     * @author Rebecca Glatts
+     * @param postfix The postfix expression to be 
+     * @return the value from the evaluated postfix expression
+     */
+    public static <T> Object evaluatePostfix(String postfix) {
         // Evaluates a postfix expression.
-        valueStack = new ResizeableArrayStack<>();
-        while (postfix has characters left to parse)
-        {
-        nextCharacter = next nonblank character of postfix
+        ResizeableArrayStack<String> valueStack = new ResizeableArrayStack<>();
+        Float operandOne;
+        Float operandTwo;
+        String result;
+        //for the length of the string, get each character and evaluate it
+        for (int i = 0; i < postfix.length(); i++) {
+            char nextCharacter = postfix.charAt(i);
 
-        switch (nextCharacter)
-        {
-        case variable:
-            valueStack.push(value of the variable nextCharacter);
-            break;
+            switch (nextCharacter) {
+                //if the character is a variable it pushes the value of the 
+                //variable to the stack
+                case 'a':
+                    valueStack.push("2");
+                    break;
+                case 'b':
+                    valueStack.push("3");
+                    break;
+                case 'c':
+                    valueStack.push("4");
+                    break;
+                case 'd':
+                    valueStack.push("5");
+                    break;
+                case 'e':
+                    valueStack.push("6");
+                    break;
+                //it adds the two top operands from the stack together if the char
+                //is a +
+                case '+':
+                    operandTwo = Float.parseFloat(valueStack.pop());
+                    operandOne = Float.parseFloat(valueStack.pop());
+                    result = String.valueOf(operandOne + operandTwo);
+                    valueStack.push(result);
+                    break;
+                //subtracts top two operands from stack if -
+                case '-':
+                    operandTwo = Float.parseFloat(valueStack.pop());
+                    operandOne = Float.parseFloat(valueStack.pop());
+                    result = String.valueOf(operandOne - operandTwo);
+                    valueStack.push(result);
+                    break;
+                //multiplies top two operands from stack if *
+                case '*':
+                    operandTwo = Float.parseFloat(valueStack.pop());
+                    operandOne = Float.parseFloat(valueStack.pop());
+                    result = String.valueOf(operandOne * operandTwo);
+                    valueStack.push(result);
+                    break;
+                //divides top two operands from stack if /
+                case '/':
+                    operandTwo = Float.parseFloat(valueStack.pop());
+                    operandOne = Float.parseFloat(valueStack.pop());
+                    result = String.valueOf(operandOne / operandTwo);
+                    valueStack.push(result);
+                    break;
+                //puts the value of operandOne to the power of operandTwo if ^
+                case '^':
+                    operandTwo = Float.parseFloat(valueStack.pop());
+                    operandOne = Float.parseFloat(valueStack.pop());
+                    result = String.valueOf(Math.pow(operandOne, operandTwo));
+                    valueStack.push(result);
+                    break;
+                //if the character is unexpected or whitespace, it ignores it
+                default:
+                    break; // Ignore unexpected characters
+            }
 
-        case '+' :
-            operandTwo = valueStack.pop();
-            operandOne = valueStack.pop();
-            result = operandOne + operandTwo;
-            valueStack.push(result);
-            break;
-        case '-' : 
-            operandTwo = valueStack.pop();
-            operandOne = valueStack.pop();
-            result = operandOne - operandTwo;
-            valueStack.push(result);
-            break;
-        case '*' : 
-            operandTwo = valueStack.pop();
-            operandOne = valueStack.pop();
-            result = operandOne * operandTwo;
-            valueStack.push(result);
-            break;
-        case '/' : 
-            operandTwo = valueStack.pop();
-            operandOne = valueStack.pop();
-            result = operandOne / operandTwo;
-            valueStack.push(result);
-            break;
-        case '^' :
-            operandTwo = valueStack.pop();
-            operandOne = valueStack.pop();
-            result = operandOne ^ operandTwo;
-            valueStack.push(result);
-            break;
+        }
+        return valueStack.peek();
 
-        default: break // Ignore unexpected characters
-}
-
-}return valueStack.peek();
-
-}
+    }
 
 }
